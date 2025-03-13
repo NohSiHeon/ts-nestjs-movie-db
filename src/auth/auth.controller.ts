26c3c66ef7291dcc821d0ab10115b4c7ce08e91f
NohSiHeon <shtlgjs016@naver.com>
2025-03-13 17:35:03 +0900

    feat: 글로벌 에러 처리 필터 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,9 @@
-import {
-  Body,
-  Controller,
-  HttpException,
-  HttpStatus,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dtos/sign-up.dto';
 import { SignUpSuccessResponseDto } from './dtos/sign-up-success-response.dto';
 import { SignUpFailResponseDto } from './dtos/sign-up-fail-response.dto';
+import { SignInDto } from './dtos/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,25 +13,23 @@ export class AuthController {
   async signUp(
     @Body() signUpDto: SignUpDto,
   ): Promise<SignUpSuccessResponseDto | SignUpFailResponseDto> {
-    try {
-      const data = await this.authService.signUp(signUpDto);
+    const data = await this.authService.signUp(signUpDto);
 
-      return {
-        status: HttpStatus.CREATED,
-        message: '회원가입 성공',
-        data,
-      };
-    } catch (error) {
-      throw new HttpException(
-        {
-          status: error.status,
-          message: error.response,
-        },
-        error.status,
-        {
-          cause: error,
-        },
-      );
-    }
+    return {
+      status: HttpStatus.CREATED,
+      message: '회원가입 성공',
+      data,
+    };
+  }
+
+  @Post('/sign-in')
+  async signIn(@Body() signInDto: SignInDto) {
+    const data = await this.authService.signIn(signInDto);
+
+    return {
+      status: HttpStatus.OK,
+      message: '로그인에 성공했습니다.',
+      data,
+    };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,14 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { AuthRepository } from './auth.repository';
 import { SignUp } from './interfaces/sign-up.interface';
 import { UsersRepository } from 'src/users/users.repository';
+import { SignInDto } from './dtos/sign-in.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,19 +22,30 @@ export class AuthService {
     const checkEmail = await this.usersRepository.findUserEmailByEmail(email);
 
     if (checkEmail) {
-      throw new HttpException('이미 가입한 이메일입니다.', HttpStatus.CONFLICT);
+      throw new ConflictException('이미 가입한 이메일입니다.');
     }
 
     if (password !== passwordConfirm) {
-      throw new HttpException(
-        '비밀번호가 일치하지 않습니다',
-        HttpStatus.BAD_REQUEST,
-      );
+      throw new BadRequestException('비밀번호가 일치하지 않습니다');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const data = await this.authRepository.signUp(name, email, hashedPassword);
 
     return data;
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const { email, password } = signInDto;
+
+    const checkEmail = await this.usersRepository.findUserEmailByEmail(email);
+
+    if (!checkEmail) {
+      throw new UnauthorizedException(
+        '이메일 혹은 비밀번호를 잘못 입력했거나 등록되지 않은 계정입니다',
+      );
+    }
+
+    return true;
   }
 }
