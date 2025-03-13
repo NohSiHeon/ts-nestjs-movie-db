1806fc414d4e3c2b6d86f3f990b9672f4798e6d7
NohSiHeon <shtlgjs016@naver.com>
2025-03-13 18:28:58 +0900

    refactor(auth): AuthModule에서 UsersModule을 import하여 UserRepository 사용 방식 변경

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma.service';
 import { AuthRepository } from './auth.repository';
-import { UsersRepository } from 'src/users/users.repository';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository, UsersRepository, PrismaService],
+  providers: [AuthService, AuthRepository, PrismaService],
 })
 export class AuthModule {}
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
 import { PrismaService } from './prisma.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [AppModule, UsersModule, AuthModule, MoviesModule],
+  imports: [
+    AppModule,
+    UsersModule,
+    AuthModule,
+    MoviesModule,
+    CacheModule.register(),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
