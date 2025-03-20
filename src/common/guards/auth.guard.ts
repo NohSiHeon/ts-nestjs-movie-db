81a82dc2f5be0869b69a8931e078062ec91d7b67
NohSiHeon <shtlgjs016@naver.com>
2025-03-20 16:39:13 +0900

    feat(movie): 영화 등록 API 구현

---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '@prisma/client';
+import { AuthenticatedUser } from '../interfaces/user.interface';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as AuthenticatedUser;
+
+    if (!user) {
+      throw new UnauthorizedException('인증되지 않은 사용자입니다.');
+    }
+
+    const requiredRoles = this.reflector.get<UserRole[]>(
+      'roles',
+      context.getHandler(),
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    return requiredRoles.includes(user.role);
+  }
+}
--- src/movies/movies.repository.ts
+++ src/movies/movies.repository.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { MovieGenre } from '@prisma/client';
 
 @Injectable()
 export class MoviesRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  // async registerMovie(
-  //   title: string,
-  //   introduction: string,
-  //   actors: string,
-  //   genre,
-  //   releaseYear: string,
-  // ) {
-  //   const data = await this.prisma.movie.create({
-  //     data: {
-  //       title,
-  //       introduction,
-  //       actors,
-  //       genre,
-  //       releaseYear,
-  //     },
-  //   });
-  //   return data;
-  // }
+  async registerMovie(
+    userId: number,
+    title: string,
+    introduction: string,
+    actors: string,
+    genre: MovieGenre,
+    releaseYear: string,
+  ) {
+    const data = await this.prisma.movie.create({
+      data: {
+        userId,
+        title,
+        introduction,
+        actors,
+        genre,
+        releaseYear,
+      },
+    });
+    return data;
+  }
 }
